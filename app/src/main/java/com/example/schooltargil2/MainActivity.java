be3cbf0811fc43d47019d239c2230b91ed1045ad
Daniel <daniel.lub2007@gmail.com>
2024-01-09 23:47:26 +0200

    1

--- app/src/main/java/com/example/schooltargil2/MainActivity.java
+++ app/src/main/java/com/example/schooltargil2/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.schooltargil2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
 
+
+public class MainActivity extends AppCompatActivity
+{
+    private Button btn;
+    private int counter;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn= findViewById(R.id.button);
+    }
+
+    public void go(View view)
+    {
+        counter++;
+        btn.setText("This is click number: " + counter);
     }
 }
